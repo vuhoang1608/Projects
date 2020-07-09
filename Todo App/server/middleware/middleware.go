6a3419d6f7683d374eb342a3bf8f71cff49b3367
John <vuhoang1608@yahoo.com>
2020-07-09 12:29:30 -0700

    Add Front End, backend and mongodb connection

---
+++ Todo App/server/middleware/middleware.go
@@ -0,0 +1,163 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"../models"
+	"github.com/gorilla/mux"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// DB connection string
+// for localhost mongoDB
+//const connectionString = "mongodb://localhost:27017"
+
+const connectionString = "mongodb+srv://admin:hoangg1608@cluster0-r74y5.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+// Database Name
+const dbName = "test"
+
+// Collection name
+const collName = "todolist"
+
+// collection object/instance
+var collection *mongo.Collection
+
+func init() {
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection = client.Database(dbName).Collection(collName)
+
+	fmt.Println("Collection instance created!")
+}
+
+// GetAllTask get all the task route
+func GetAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	payload := getAllTask()
+	json.NewEncoder(w).Encode(payload)
+}
+
+// get all task from the DB and return it
+func getAllTask() []primitive.M {
+	cur, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var results []primitive.M
+	for cur.Next(context.Background()) {
+		var result bson.M
+		e := cur.Decode(&result)
+		if e != nil {
+			log.Fatal(e)
+		}
+		// fmt.Println("cur..>", cur, "result", reflect.TypeOf(result), reflect.TypeOf(result["_id"]))
+		results = append(results, result)
+
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cur.Close(context.Background())
+	return results
+}
+
+// CreateTask create task route
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	var task models.ToDoList
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	// fmt.Println(task, r.Body)
+	insertOneTask(task)
+	json.NewEncoder(w).Encode(task)
+}
+
+// Insert one task in the DB
+func insertOneTask(task models.ToDoList) {
+	insertResult, err := collection.InsertOne(context.Background(), task)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted a Single Record ", insertResult.InsertedID)
+}
+
+// DeleteTask delete one task route
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	params := mux.Vars(r)
+	deleteOneTask(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+	// json.NewEncoder(w).Encode("Task not found")
+}
+
+// delete one task from the DB, delete by ID
+func deleteOneTask(task string) {
+	fmt.Println(task)
+	id, _ := primitive.ObjectIDFromHex(task)
+	filter := bson.M{"_id": id}
+	d, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Document", d.DeletedCount)
+}
+
+// DeleteAllTask delete all tasks route
+func DeleteAllTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	count := deleteAllTask()
+	json.NewEncoder(w).Encode(count)
+	// json.NewEncoder(w).Encode("Task not found")
+
+}
+
+// delete all the tasks from the DB
+func deleteAllTask() int64 {
+	d, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Deleted Document", d.DeletedCount)
+	return d.DeletedCount
+}
